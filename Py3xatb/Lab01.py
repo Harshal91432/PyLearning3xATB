232d86bb67af9ae3db79f93f30014009c15464e0
Harshal91432 <hpb1432@gmail.com>
2024-11-03 22:13:23 +0530

    Area of Circle

--- Py3xatb/Lab01.py
+++ Py3xatb/Lab01.py
@@ -2,8 +2,5 @@ print("Harsh")
 print(3+4)
 print(2.4)
 print(3/4)
-print(3*7)
-
-
-
+print(3 * 7)
 
---
+++ Py3xatb/Lab02.py
@@ -0,0 +1,13 @@
+a = 6
+b = 6
+print(a==b)
+print(3**2)
+print(3^5)
+
+
+#Area of Circle
+a = int(input("enter a number: "))
+
+b = ('Area_of_circle', 3.14*(a**2))
+
+print (b)
